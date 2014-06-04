702d20237f78b4ec682e96252e8fd5eb7a711496
unknown <pdiffenderfer@BCFMPAISXD4F.benecard.local>
2014-06-04 15:13:27 -0400

    Fixed getDistanceAndNormal in Segment.

--- Java/src/org/magnos/steer/obstacle/Segment.java
+++ Java/src/org/magnos/steer/obstacle/Segment.java
@@ -34,37 +34,79 @@ public class Segment<V extends Vec<V>> implements Obstacle<V>
     @Override
     public float getDistanceAndNormal( V origin, V lookahead, V outNormal )
     {
-        V a = start;
-        V b = end.sub( start );
-        V c = origin;
-        V e = lookahead.sub( origin );
-        V u = a.sub( c );
-
-        float ub = u.dot( b );
-        float ee = e.dot( e );
-        float ue = u.dot( e );
-        float be = b.dot( e );
-        float bb = b.dot( b );
-        float denom = be * be - bb * ee;
-
-        // parallel or coincident
-        if ( denom == 0.0f )
-        {
-            return SteerMath.closest( a, b, c, outNormal ).subi( c ).normalize() - thickness;
+//        V center = start.interpolateTo( end, 0.5f );
+//        
+//        V closest = SteerMath.closest( start, end, lookahead, outNormal );
+//        
+//        float distance = closest.length() - thickness;
+//        
+//        outNormal.set( lookahead ).subi( center );
+//        
+//        if (center != null)
+//        {
+//            return distance * 2;
+//        }
+        
+        V u = end.sub( start );
+        V v = lookahead.sub( origin );
+        V w = start.sub( origin );
+        float a = u.dot( u );
+        float b = u.dot( v );
+        float c = v.dot( v );
+        float d = u.dot( w );
+        float e = v.dot( w );
+        float D = a * c - b * b;
+        float sc, sN, sD = D;
+        float tc, tN, tD = D;
+        
+        // parallel
+        if (D < SteerMath.EPSILON) {
+            sN = 0.0f;
+            sD = 1.0f;
+            tN = e;
+            sD = c;
+        } else {
+            sN = (b * e - c * d);
+            tN = (a * e - b * d);
+            if (sN < 0.0f) {    
+                sN = 0.0f;
+                tN = e;
+                tD = c;
+            } else if (sN > sD) {
+                sN = sD;
+                tN = e + b;
+                tD = c;
+            }
         }
+        
+        if (tN < 0.0) { 
+            tN = 0.0f;
+            if (-d < 0.0f) {
+                sN = 0.0f;
+            } else if (-d > a) {
+                sN = sD;
+            } else {
+                sN = -d;
+                sD = a;
+            }
+        }  else if (tN > tD) {
+            tN = tD;
+            if ((-d + b) < 0.0f) {
+                sN = 0.0f;
+            } else if ((-d + b) > a) {
+                sN = sD;
+            } else {
+                sN = (-d +  b);
+                sD = a;
+            }
+        }
+        
+        sc = (Math.abs(sN) < SteerMath.EPSILON ? 0.0f : sN / sD);
+        tc = (Math.abs(tN) < SteerMath.EPSILON ? 0.0f : tN / tD);
 
-        // skew
-        float invdenom = 1.0f / denom;
-        float s = (ub * ee - ue * be) * invdenom;
-        float t = (ub * be - ue * bb) * invdenom;
-
-        float sclamped = SteerMath.clamp( s, 0.0f, 1.0f );
-        float tclamped = SteerMath.clamp( t, 0.0f, 1.0f );
-
-        V ab = outNormal.interpolate( a, b, sclamped );
-        V ce = outNormal.interpolate( c, e, tclamped );
-
-        return outNormal.directi( ce, ab ).normalize() - thickness;
+        // dP = w + (sc * u) - (tc * v);
+        
+        return outNormal.set( w ).addsi( u, sc).addsi( v, -tc ).normalize() - thickness;
     }
 
     @Override
@@ -76,7 +118,7 @@ public class Segment<V extends Vec<V>> implements Obstacle<V>
     @Override
     public float getRadius()
     {
-        return start.distance( end ) * 0.5f;
+        return start.distance( end ) * 0.5f + thickness * 2;
     }
 
 }
